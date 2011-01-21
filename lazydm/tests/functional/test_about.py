65055012f3c7cfa931868cc80aa7658705f86e9a
Zero <zotthewizard@gmail.com>
2011-01-21 00:02:06 -0500

    Removed about template and controllers

--- lazydm/tests/functional/test_about.py
+++
@@ -1,7 +0,0 @@
-from lazydm.tests import *
-
-class TestAboutController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='about', action='index'))
-        # Test response...
